ad8ef3b580dc62870a232689611bdaf18dbc35f4
miladimos <milad.jafari6210@gmail.com>
2021-09-26 20:29:42 +0330

    edit config

--- config/attachmentable.php
+++ config/attachmentable.php
@@ -2,5 +2,20 @@
 
 // config file for laravelir/attachmentable
 return [
-    //
+    // All of Functionality in this Directory
+    'base_directory' => env("ATTACHMENTABLE_BASE_DIR", 'attachments'),
+
+    /**
+     * List of disk names that you want to use for upload
+     *
+     * local, public
+     *
+     */
+    'disk' => env('ATTACHMENTABLE_DISK', 'local'),
+
+    /**
+     * The maximum upload file size of an item in bytes.
+     * Adding a larger file will result in an exception.
+     */
+    'max_file_size' => 1024 * 1024 * 10,
 ];
